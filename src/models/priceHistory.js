f95f79466245c0d02f932ace9e8bf6b7afc0b722
MichaelDieterle <michael.dieterle.online@gmail.com>
2026-07-23 08:36:21 +0200

    fix: add unique constraint on (stockId, date) to prevent duplicate price entries

--- src/models/priceHistory.js
+++ src/models/priceHistory.js
@@ -1,13 +1,18 @@
-﻿module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes) => {
   const PriceHistory = sequelize.define('PriceHistory', {
     date: { type: DataTypes.DATEONLY, allowNull: false },
     open: { type: DataTypes.FLOAT, allowNull: false },
     high: { type: DataTypes.FLOAT, allowNull: false },
     low: { type: DataTypes.FLOAT, allowNull: false },
     close: { type: DataTypes.FLOAT, allowNull: false },
-    volume: { type: DataTypes.BIGINT, allowNull: false },
-    adjClose: { type: DataTypes.FLOAT, field: 'adj_close' },
-  }, { timestamps: false });
+    volume: DataTypes.BIGINT,
+    adjClose: DataTypes.FLOAT,
+  }, {
+    timestamps: false,
+    indexes: [
+      { unique: true, fields: ['stockId', 'date'] }
+    ],
+  });
 
   PriceHistory.associate = (models) => {
     PriceHistory.belongsTo(models.Stock, { foreignKey: 'stockId', as: 'stock' });
