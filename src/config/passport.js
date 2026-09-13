89a66dca9a032b86de156a0a5252e9401467cc2c
MichaelDieterle <michael.dieterle.online@gmail.com>
2026-09-13 03:39:14 +0200

    fix: make Google account email handling robust

--- src/config/passport.js
+++ src/config/passport.js
@@ -10,17 +10,18 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        // Find or create user based on googleId
-        let user = await User.findOne({ where: { googleId: profile.id } });
+        const email = profile.emails?.[0]?.value;
+        if (!email) return done(new Error('Google account did not provide an email address'));
 
+        let user = await User.findOne({ where: { googleId: profile.id } });
         if (!user) {
-          // Create new user
-          // We use the google profile email as username
-          user = await User.create({
-            username: profile.emails[0].value,
-            googleId: profile.id,
-            role: 'user'
-          });
+          user = await User.findOne({ where: { username: email } });
+        }
+        if (!user) {
+          user = await User.create({ username: email, googleId: profile.id, role: 'user' });
+        } else if (!user.googleId) {
+          user.googleId = profile.id;
+          await user.save();
         }
         return done(null, user);
       } catch (err) {
