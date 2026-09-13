3f0b5a87c34d07418d531075a42df3abb666acdb
MichaelDieterle <michael.dieterle.online@gmail.com>
2026-09-13 04:50:50 +0200

    Remove authentication gate from frontend

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,12 +2,10 @@ import './style.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import AuthGate from './components/AuthGate'
 
 const root = createRoot(document.querySelector<HTMLDivElement>('#app')!)
 root.render(
   React.createElement(React.StrictMode, null,
-    React.createElement(AuthGate),
     React.createElement(App)
   )
 )
