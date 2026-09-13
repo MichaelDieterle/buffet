34b3821c8bf3b2e65208841d94ccbc6560e34125
MichaelDieterle <michael.dieterle.online@gmail.com>
2026-09-13 04:23:26 +0200

    feat(auth): make manual username login the frontend entry point

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,10 +2,12 @@ import './style.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import AuthGate from './components/AuthGate'
 
 const root = createRoot(document.querySelector<HTMLDivElement>('#app')!)
 root.render(
   React.createElement(React.StrictMode, null,
+    React.createElement(AuthGate),
     React.createElement(App)
   )
 )
